16023b6c21ad503134a19d10f00360791859458c
Avram <avramkjiprovski@gmail.com>
2023-12-16 22:53:53 +0100

    Small Image update

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -4,14 +4,20 @@ interface ImageProps {
   src: string;
   alt: string;
   imageSize?: "large" | "medium" | "small";
-  overlayColor?: string;
+  overlayColor?: boolean;
 }
 
 const Image = ({ src, alt, imageSize = 'large', overlayColor }: ImageProps) => {
   const imageClasses = `Image Image-${imageSize.charAt(0).toUpperCase()+imageSize.slice(1)}`
 
   return (
-    <img className={imageClasses} src={src} alt={alt} />
+    <div>
+      <div
+        className={imageClasses}
+      >
+        <img src={src} alt={alt} />
+      </div>
+    </div>
   );
 };
 
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,28 @@
+import Image from '../Image/Image';
+import TitleAndText from '../TitleAndText/TitleAndText';
 import './Main.css';
+import PhoneAndKeyboardImage from '../../assets/desktop/image-phone-and-keyboard.jpg'
+import ImageGlassAndKeyboard from '../../assets/desktop/image-glass-and-keyboard.jpg'
 
 const Main = () => {
   return (
     <div className="Main">
-      <h1>Main section</h1>
-      <p>To be continued... </p>
+      <Image
+        src={PhoneAndKeyboardImage}
+        alt='phone and keyboard'
+        imageSize='small'
+        overlayColor
+      />
+      <Image
+        src={ImageGlassAndKeyboard}
+        alt='glass and keyboard'
+        imageSize='medium'
+      />
+      <TitleAndText 
+        h2
+        title='Mechanical wireless keyboard'
+        text='The Typemaster keyboard boasts top-notch build and practical design. It offers a wide variety of switches and keycaps, along with realiable wireless connectivity.'
+      />
     </div>
   );
 }
